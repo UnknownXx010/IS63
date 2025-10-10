e9d592f239f57f8f480d3d85f4830f8baac691cf
UnknownXx010 <athag90@gmail.com>
2025-10-10 09:33:45 +0700

    if

--- logika.php
+++ logika.php
@@ -7,17 +7,31 @@
 </head>
 <body>
   <?php 
+  $username ="atha123";
+  $password ="asd";
+  
+  if($username == "atha123" && $password =="atha27"){
+   echo "$username benar  dan password benar, login kamu berhasil";
+  }elseif($username !="atha123" && $password =="atha27"){
+    echo "$username salah ,password benar";
+  }elseif($username =="atha123" && $password !="atha27"){
+    echo "$username benar ,password salah";
+  }else{
+    echo "lu GOBLOK ITU AJA LUPA";
+  }
+  
 
-$nama ="higan";
-$nilai =100;
 
-if($nilai >=70){
-  echo "<h2>$nama,selamat kamu lulus </h2>";
-}elseif($nilai >=40 ){
-  echo "<h2>$nama,maaf belum lulus </h2>";
-}else{
-  echo "<h2>$nama,hati hati kamu remedial </h2>";
-}
+// $nama ="higan";
+// $nilai =100;
+
+// if($nilai >=70){
+//   echo "<h2>$nama,selamat kamu lulus </h2>";
+// }elseif($nilai >=40 ){
+//   echo "<h2>$nama,maaf belum lulus </h2>";
+// }else{
+//   echo "<h2>$nama,hati hati kamu remedial </h2>";
+// }
     
    
   
