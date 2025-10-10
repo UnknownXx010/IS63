edd280e5f6554a1d3110d25855b733fd15dd1c3d
UnknownXx010 <athag90@gmail.com>
2025-10-10 08:35:20 +0700

    logika

---
+++ logika.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+
+  $nilai =70;
+  if($nilai >50);{
+   echo "kamu lulus";
+  }
+  ?>
+
+</body>
+</html>
